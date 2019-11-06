ba567c31194b6c6c142110559da604311aaa630a
toasty ghosty <fionasmialek13@gmail.com>
2019-11-06 15:24:16 -0600

    added moto inventory

---
+++ moto-inventory-service/src/main/java/com/example/motoinventoryservice/MotoInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.motoinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoInventoryServiceApplication.class, args);
+	}
+
+}
